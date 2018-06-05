896e5fd3a5c2b5976da029f393c4a72bface471c
Evaline <evalinebai@gmail.com>
2018-06-05 10:28:08 -0700

    Complete encoding.js

--- code/part-two/client/source/services/encoding.js
+++ code/part-two/client/source/services/encoding.js
@@ -13,9 +13,28 @@
  *   deterministically! JSON is convenient, but you will need to sort
  *   your object's keys or random transactions may fail.
  */
-export const encode = object => {
-  // Enter your solution here
 
+const concatMap = (array, iterator) => {
+  return array.reduce((mapped, item) => {
+    return mapped.concat(iterator(item));
+  }, []);
+};
+
+const listKeys = object => {
+  if (!object || typeof object !== 'object') {
+    return [];
+  }
+  if (Array.isArray(object)) {
+    return concatMap(object, item => listKeys(item));
+  }
+  const topKeys = Object.keys(object);
+  const nestedKeys = concatMap(topKeys, key => listKeys(object[key]));
+  return topKeys.concat(nestedKeys);
+};
+
+export const encode = object => {
+  const sortedKeys = listKeys(object).sort();
+  return Buffer.from(JSON.stringify(object, sortedKeys));
 };
 
 /**
@@ -27,6 +46,5 @@ export const encode = object => {
  *   base64 string -> Buffer -> JSON string -> object
  */
 export const decode = base64Str => {
-  // Your code here
-
+  return JSON.parse(Buffer.from(base64Str, 'base64').toString());
 };
